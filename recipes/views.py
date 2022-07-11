5459f3da21d386e0411f9868d472dcf0a3c11bb7
Matheus Morais <mth34697@gmail.com>
2022-07-11 13:04:28 -0300

    Configurando o apps e urls do django

---
+++ recipes/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+
+# from django.shortcuts import render
+# from django.urls import path
+
+
+# Create your views here.
+def home(request):
+    return HttpResponse('Home')
+
+
+def sobre(request):
+    return HttpResponse('Sobre')
+
+
+def contato(request):
+    return HttpResponse('Contato')
