98be2e3140756256efc40f976401a302c1cc2afe
Matheus Morais <mth34697@gmail.com>
2022-07-19 14:07:49 -0300

    Alterações de teste inicial

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
-# from django.shortcuts import render
 # from django.urls import path
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Home')
+    return render(request, 'recipes/home.html')
 
 
 def sobre(request):
