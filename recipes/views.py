65b007f6d1f6219f0548d269affe10e331d691a2
Matheus Morais <mth34697@gmail.com>
2022-07-25 09:31:41 -0300

    Removido o contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
 # from django.urls import path
@@ -7,11 +7,3 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'recipes/home.html')
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
-
-
-def contato(request):
-    return HttpResponse('Contato')
